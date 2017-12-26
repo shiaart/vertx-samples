b8a4756996cf3793ca401bf6aade0d705d22b21a
Art Sh <shiabutdinov@gmail.com>
2017-12-26 22:17:01 +0300

    test github desktop

--- src/main/java/com/aow/server/MainVerticle.java
+++ src/main/java/com/aow/server/MainVerticle.java
@@ -9,7 +9,7 @@ public class MainVerticle extends AbstractVerticle {
         vertx.createHttpServer().requestHandler(req -> {
               req.response()
                 .putHeader("content-type", "text/plain")
-                .end("Hello from Vert.x!");
+                .end("Hello from Vert.xxxxxxxxxxx!");
             }).listen(8080);
         System.out.println("HTTP server started on port 8080");
     }
